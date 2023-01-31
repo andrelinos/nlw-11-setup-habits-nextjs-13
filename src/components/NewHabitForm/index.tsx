e024ec2851c7438a14f9208b09e15e2ee58565eb
Andrelino Silva <andrelinodev@gmail.com>
2023-01-31 15:18:55 -0300

    Update

--- src/components/NewHabitForm/index.tsx
+++ src/components/NewHabitForm/index.tsx
@@ -41,6 +41,18 @@ export function NewHabitForm() {
             //     title,
             //     weekDays,
             // });
+            // fetch('http://localhost:3000/api/habits', {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            // })
+            //     .then((response) => response.json())
+            //     .then((data) => {
+
+            //         console.log(data);
+            //     })
+            //     .catch((error) => console.error('Algo deu ruim!'));
 
             setTitle('');
             setWeekDays([]);
